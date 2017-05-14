8a6e4e51ca2f6b0dd80987d515994b8b72c6142b
mhelvens <mhelvens@gmail.com>
2017-05-14 14:56:55 +0100

    added babel-plugin-syntax-decorators dependency

--- built.js
+++ built.js
@@ -6,11 +6,18 @@ Object.defineProperty(exports, "__esModule", {
 
 exports.default = function () {
     return {
+        inherits: _babelPluginSyntaxDecorators2.default,
         visitor: _defineProperty({}, decoratableTypes.join('|'), function (path) {
             path.node.decorators = null;
         })
     };
 };
+
+var _babelPluginSyntaxDecorators = require("babel-plugin-syntax-decorators");
+
+var _babelPluginSyntaxDecorators2 = _interopRequireDefault(_babelPluginSyntaxDecorators);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
